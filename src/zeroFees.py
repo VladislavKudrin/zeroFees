3eda99ff7106c66a7a51e0931174aa52a1d8f925
Vladislav Kudrin <vladislavkudrin@gmail.com>
2024-08-19 21:02:38 +0200

    <ore flexibility / .env file

--- src/zeroFees.py
+++ src/zeroFees.py
@@ -1,17 +1,22 @@
+import json
 from blockfrost import ApiUrls
 from pycardano import Address, Network, PaymentVerificationKey, BlockFrostChainContext, HDWallet, TransactionInput, TransactionOutput, Value, utils, TransactionBody, PaymentExtendedSigningKey, VerificationKeyWitness, PaymentExtendedVerificationKey, Transaction, TransactionWitnessSet
 import requests
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
-FEE = 5000000
-SEND_AMOUNT = 100
-FEE_IN_TOKENS = 4
-TOKEN_POLICY_ID = ''
-TOKEN_NAME = ''
-BLOCKFROST_PROJECT_ID = ''
-MNEMONIC_SEND = ""
-MNEMONIC_FEE_PAYER = ""
-TO_ADDRESS = ''
+BLOCKFROST_PROJECT_ID = os.getenv("BLOCKFROST_PROJECT_ID")
+FEE = int(os.getenv("FEE"))
+SEND_AMOUNT = int(os.getenv("SEND_AMOUNT"))
+FEE_IN_TOKENS = int(os.getenv("FEE_IN_TOKENS"))
+TOKEN_POLICY_ID = os.getenv("TOKEN_POLICY_ID")
+TOKEN_NAME = os.getenv("TOKEN_NAME")
+MNEMONIC_SEND = os.getenv("MNEMONIC_SEND")
+MNEMONIC_FEE_PAYER = os.getenv("MNEMONIC_FEE_PAYER")
+TO_ADDRESS = os.getenv("TO_ADDRESS")
+
 
 def get_address_utxos(address):
   headers = {
@@ -21,37 +26,16 @@ def get_address_utxos(address):
   response = requests.get("{blockfrost_url}/addresses/{address}/utxos".format(blockfrost_url="https://cardano-preprod.blockfrost.io/api/v0", address=address), headers=headers)
   return response.json()
 
-def get_valid_sender_utxos(address, unit, token_amount, fee_amount):
+def get_valid_utxos(address, unit, token_amount):
   utxos = get_address_utxos(address=address)
-  valid_utxos = {
-    "token": [],
-    "lovelace": []
-  }
+  valid_utxos = []
   
   for utxo in utxos:
     for amount in utxo['amount']:
-      if amount['unit'] == 'lovelace' and int(amount['quantity']) >= int(fee_amount):
-        valid_utxos["lovelace"].append(utxo)
       if amount['unit'] == unit and int(amount['quantity']) >= int(token_amount):
-        valid_utxos["token"].append(utxo)
-
-  return {
-    "token": valid_utxos["token"][0],
-    "lovelace": valid_utxos["lovelace"][0]
-  }
-
-def get_valid_fee_utxos(address):
-  utxos = get_address_utxos(address=address)
-  valid_utxos = {
-    "lovelace": []
-  }
+        valid_utxos.append(utxo)
   
-  for utxo in utxos:
-    for amount in utxo['amount']:
-      if amount['unit'] == 'lovelace' and int(amount['quantity']) >= FEE:
-        valid_utxos["lovelace"].append(utxo)
-
-  return valid_utxos
+  return valid_utxos[0]
 
 def calculate_fee_for_output(address, policyId, tokenName, amount):
   if policyId and tokenName:
@@ -111,16 +95,16 @@ to_address = Address.decode(TO_ADDRESS)
 send_address_fee = calculate_fee_for_output(to_address, TOKEN_POLICY_ID, TOKEN_NAME, SEND_AMOUNT + FEE_IN_TOKENS)
 
 #GET VALID SENDER UTXOS
-valid_sender_utxos = get_valid_sender_utxos(send_address, TOKEN_POLICY_ID+TOKEN_NAME, SEND_AMOUNT + FEE_IN_TOKENS, send_address_fee)
+valid_sender_utxo = get_valid_utxos(send_address, TOKEN_POLICY_ID+TOKEN_NAME, SEND_AMOUNT + FEE_IN_TOKENS)
 
 #DEFINE SENDER INPUTS
-tx_in_sender_inputs = TransactionInput.from_primitive([valid_sender_utxos["token"]["tx_hash"], valid_sender_utxos["token"]["tx_index"]])
+tx_in_sender_inputs = TransactionInput.from_primitive([valid_sender_utxo["tx_hash"], valid_sender_utxo["tx_index"]])
 
 #GET INPUT FROM FEE PAY ADDRESS
-valid_fee_utxos = get_valid_fee_utxos(fee_address)
+valid_fee_utxo = get_valid_utxos(fee_address, "lovelace", FEE)
 
 #DEFINE FEE PAYER INPUTS
-tx_in_fee_input = TransactionInput.from_primitive([valid_fee_utxos["lovelace"][0]["tx_hash"], valid_fee_utxos["lovelace"][0]["tx_index"]])
+tx_in_fee_input = TransactionInput.from_primitive([valid_fee_utxo["tx_hash"], valid_fee_utxo["tx_index"]])
 
 # #DEFINE OUTPUTS
 send_address_output_tokens = TransactionOutput(to_address, Value.from_primitive([
@@ -132,41 +116,49 @@ send_address_output_tokens = TransactionOutput(to_address, Value.from_primitive(
         },
     ]))
 
+formatted_json = json.dumps(valid_sender_utxo, indent=4)
+
 send_address_change = TransactionOutput(send_address, Value.from_primitive([
-        int(valid_sender_utxos["token"]["amount"][0]["quantity"]), 
+        sum(int(amount["quantity"]) for amount in valid_sender_utxo["amount"] if amount["unit"] == "lovelace"),
         {
             TOKEN_POLICY_ID: {
-                TOKEN_NAME: int(valid_sender_utxos["token"]["amount"][1]["quantity"]) - SEND_AMOUNT - FEE_IN_TOKENS,
+                TOKEN_NAME: sum(int(amount["quantity"]) for amount in valid_sender_utxo["amount"] if amount["unit"] == TOKEN_POLICY_ID+TOKEN_NAME) - SEND_AMOUNT - FEE_IN_TOKENS,
             }
         },
     ]))
 
 #DEFINE FEE TEMPLATE TO CALCULATE FEE
-fee_address_change_template = TransactionOutput(fee_address, Value.from_primitive([
-        int(valid_fee_utxos["lovelace"][0]["amount"][0]["quantity"]), 
+fee_address_change_template = TransactionOutput(send_address, Value.from_primitive([
+        sum(int(amount["quantity"]) for amount in valid_fee_utxo["amount"] if amount["unit"] == "lovelace"),
         {
-            valid_fee_utxos["lovelace"][0]["amount"][1]["unit"][:56]: {
-              valid_fee_utxos["lovelace"][0]["amount"][1]["unit"][56:]: int(valid_fee_utxos["lovelace"][0]["amount"][1]["quantity"]),
-            },
-            TOKEN_POLICY_ID : {
-              TOKEN_NAME: FEE_IN_TOKENS
+          **{
+            amount["unit"][:56]: {
+                amount["unit"][56:]: int(amount["quantity"])
             }
-        },
+            for amount in valid_fee_utxo["amount"]
+            if amount["unit"] != "lovelace" and amount["unit"] != TOKEN_POLICY_ID + TOKEN_NAME
+          },
+          TOKEN_POLICY_ID: {
+              TOKEN_NAME: sum(int(amount["quantity"]) for amount in valid_fee_utxo["amount"] if amount["unit"] == TOKEN_POLICY_ID + TOKEN_NAME) + FEE_IN_TOKENS
+          }},
     ]))
 
 tx_fee = calculate_tx_fee([tx_in_sender_inputs, tx_in_fee_input], [send_address_output_tokens, send_address_change, fee_address_change_template])
 
 #REAL FEE OUTPUT
 fee_address_change = TransactionOutput(fee_address, Value.from_primitive([
-        int(valid_fee_utxos["lovelace"][0]["amount"][0]["quantity"]) - tx_fee - send_address_fee, 
+        sum(int(amount["quantity"]) for amount in valid_fee_utxo["amount"] if amount["unit"] == "lovelace") - tx_fee - send_address_fee, 
         {
-            valid_fee_utxos["lovelace"][0]["amount"][1]["unit"][:56]: {
-              valid_fee_utxos["lovelace"][0]["amount"][1]["unit"][56:]: int(valid_fee_utxos["lovelace"][0]["amount"][1]["quantity"]),
-            },
-            TOKEN_POLICY_ID : {
-              TOKEN_NAME: FEE_IN_TOKENS
+          **{
+            amount["unit"][:56]: {
+                amount["unit"][56:]: int(amount["quantity"])
             }
-        },
+            for amount in valid_fee_utxo["amount"]
+            if amount["unit"] != "lovelace" and amount["unit"] != TOKEN_POLICY_ID + TOKEN_NAME
+          },
+          TOKEN_POLICY_ID: {
+              TOKEN_NAME: sum(int(amount["quantity"]) for amount in valid_fee_utxo["amount"] if amount["unit"] == TOKEN_POLICY_ID + TOKEN_NAME) + FEE_IN_TOKENS
+          }},
     ]))
 
 #REAL TX
@@ -178,6 +170,6 @@ vk_witnesses = [VerificationKeyWitness(spend_vk_fee, fee_address_signature)]
 signed_tx = Transaction(tx_body, TransactionWitnessSet(vkey_witnesses=vk_witnesses))
 
 
-#CBOR FOR ETERNL
+# #CBOR FOR ETERNL
 print(signed_tx.to_cbor().hex())
 
